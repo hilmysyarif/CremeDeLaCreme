f5eb406210471a50ae66972a325585523ebeab0b
jeremybellaiche <contact@jeremybellaiche.com>
2015-05-03 14:58:32 +0200

    Login style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
                     @if(Session::has('error'))
                             {{Session::get('error')}}
                     @endif
-                     {!!Form::open(['url' => 'auth/login', 'class' => 'form-horizontal'])!!}
+                     {!!Form::open(['url' => 'admin/login', 'class' => 'form-horizontal'])!!}
 								{!!Form::email('email', null, ['class' => 'form-control', 'required' => true, 'placeholder' => 'Email'])!!}
 							{!!Form::password('password', ['class' => 'form-control', 'required' => true, 'placeholder' => 'Mot de passe'])!!}
 									{!!Form::checkbox('remember')!!} Se souvenir de moi
