abaedb264c70cdbc541e65fd0a0714ddcee65c4b
jeremybellaiche <contact@jeremybellaiche.com>
2015-05-03 16:51:05 +0200

    Login working with tests

--- resources/views/auth/app.blade.php
+++ resources/views/auth/app.blade.php
@@ -1,5 +1,94 @@
+<!DOCTYPE html>
+<html>
 
+<head>
+  <meta charset="utf-8">
+  <title>Crème de la Crème</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700'>
+  <!-- Theme CSS -->
+  @yield('css')
+	{!! HTML::style('assets/skin/default_skin/css/theme.css') !!}
+	{!! HTML::style('assets/admin-tools/admin-forms/css/admin-forms.css') !!}
 
-	@yield('content')
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+   <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+   <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+   <![endif]-->
+</head>
+<body class="external-page sb-l-c sb-r-c">
+
+   <!-- Start: Main -->
+  <div id="main" class="animated fadeIn">
+
+    <!-- Start: Content-Wrapper -->
+    <section id="content_wrapper">
+
+      <!-- Begin: Content -->
+      <section id="content">
+
+        <div class="admin-form theme-info" id="login1">
+
+          <div class="row mb15 table-layout">
+
+            <div class="col-xs-6 va-m pln">
+                <h1 class="logo" >La Crème de la Crème</h1>
+            </div>
+
+          </div>
+
+          <div class="panel panel-info mt10 br-n">
+
+        	@yield('content')
+
+            
+          </div>
+        </div>
+
+      </section>
+      <!-- End: Content -->
+
+    </section>
+    <!-- End: Content-Wrapper -->
+
+  </div>
+  <!-- End: Main -->
+
+  <!-- BEGIN: PAGE SCRIPTS -->
+
+  <!-- jQuery -->
+  <script src="vendor/jquery/jquery-1.11.1.min.js"></script>
+  <script src="vendor/jquery/jquery_ui/jquery-ui.min.js"></script>
+
+  <!-- CanvasBG Plugin(creates mousehover effect) -->
+  <script src="vendor/plugins/canvasbg/canvasbg.js"></script>
+
+  <!-- Theme Javascript -->
+  <script src="assets/js/utility/utility.js"></script>
+  <script src="assets/js/demo/demo.js"></script>
+  <script src="assets/js/main.js"></script>
+
+  <!-- Page Javascript -->
+  <script type="text/javascript">
+  jQuery(document).ready(function() {
+
+    "use strict";
+
+    // Init Theme Core      
+    Core.init();
+
+    // Init Demo JS
+    Demo.init();
+
+  
+  });
+  </script>
+
+  <!-- END: PAGE SCRIPTS -->
+
+</body>
+
+</html>
 
 	
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,13 +1,53 @@
 @extends('auth.app')
-
 @section('content')
-                    @if(Session::has('error'))
-                            {{Session::get('error')}}
-                    @endif
-                     {!!Form::open(['url' => 'admin/login', 'class' => 'form-horizontal'])!!}
-								{!!Form::email('email', null, ['class' => 'form-control', 'required' => true, 'placeholder' => 'Email'])!!}
-							{!!Form::password('password', ['class' => 'form-control', 'required' => true, 'placeholder' => 'Mot de passe'])!!}
-									{!!Form::checkbox('remember')!!} Se souvenir de moi
-							    {!! Form::submit('Se connecter', ['class' => 'btn btn-auth']) !!}
-					{!!Form::close()!!}
-@endsection
+			@if(Session::has('error'))
+			<div class="panel-heading heading-border bg-white">
+              	<div class="alert alert-danger">
+              		{{ Session::get('error')}}
+              	</div>
+            </div>
+            @endif
+
+			<!-- end .form-header section -->
+            {!!Form::open(['url' => 'admin/login', 'id' => 'contact'])!!}
+              <div class="panel-body bg-light p30">
+                <div class="row">
+                  <div class="col-sm-12 pr30">
+					
+                    <div class="section">
+                      <label for="email" class="field-label text-muted fs18 mb10">Adresse e-mail</label>
+                      <label for="email" class="field prepend-icon">
+						{!!Form::email('email', null, ['class' => 'form-control', 'required' => true, 'placeholder' => 'Email'])!!}
+                        <label for="email" class="field-icon">
+                          <i class="fa fa-user"></i>
+                        </label>
+                      </label>
+                    </div>
+                    <!-- end section -->
+
+                    <div class="section">
+                      <label for="username" class="field-label text-muted fs18 mb10">Mot de passe</label>
+                      <label for="password" class="field prepend-icon">
+                        {!!Form::password('password', ['class' => 'form-control', 'required' => true, 'placeholder' => 'Mot de passe'])!!}
+                        <label for="password" class="field-icon">
+                          <i class="fa fa-lock"></i>
+                        </label>
+                      </label>
+                    </div>
+                    <!-- end section -->
+
+                  </div>
+                </div>
+              </div>
+              <!-- end .form-body section -->
+              <div class="panel-footer clearfix p10 ph15">
+                <label class="switch ib switch-primary pull-left input-align mt10">
+                  <input type="checkbox" name="remember" id="remember" checked>
+                  <label for="remember" data-on="OUI" data-off="NON"></label>
+                  <span>Garder ma session active</span>
+                </label>
+                <button type="submit" class="button btn-primary mr10 pull-right">Connexion</button>
+              </div>
+              <!-- end .form-footer section -->
+            </form>
+@stop
